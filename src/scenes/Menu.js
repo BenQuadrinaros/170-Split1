106d3ece403b8e64042ef05b8e48f201d394177e
Carl Erez <cerez@ucsc.edu>
2020-10-17 10:39:19 -0700

    Initial menu and settings functional

--- src/scenes/Menu.js
+++ src/scenes/Menu.js
@@ -5,43 +5,40 @@ class Menu extends Phaser.Scene {
 
     preload() {
         //load audio files
+
+        //load images
+        this.load.image('TitleScreen', './assets/TempTitle.png');
+        this.load.spritesheet('Play','./assets/PlayInitial.png',{frameWidth: 130, frameHeight: 66, startFrame:0 , endFrame: 1});
+        this.load.spritesheet('Settings','./assets/SettingsInitial.png',{frameWidth: 158, frameHeight: 50, startFrame:0 , endFrame: 1});
     }
     
     create() {
-        let menuConfig = {
-            fontFamily: "Courier", 
-            fontSize: "26px",
-            backgroundColor: "#F3B141",
-            color: "#843605",
-            align: "right",
-            padding: {
-                top: 5,
-                bottom: 5
-            },
-            fixedWidth: 0
-        };
 
         let centerX = game.config.width/2;
         let centerY = game.config.height/2;
         let textSpacer = 64;
 
-        this.add.text(centerX, centerY - textSpacer, "ROCKET PATROL", menuConfig).setOrigin(.5);
-        this.add.text(centerX, centerY, "Use <- -> arrows to move and (F) to Fire", menuConfig).setOrigin(.5);
-        menuConfig.backgroundColor = "#00FF00";
-        menuConfig.color = "#000";
-        this.add.text(centerX, centerY + textSpacer, "Press <- for Easy and  -> for Hard", menuConfig).setOrigin(.5);
-
+        //Setting controls
         keyLEFT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.LEFT);
         keyRIGHT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.RIGHT);
 
-        //launches next scene
-        //this.scene.start("playScene");
+        //Setting Background
+        this.menu = this.add.image(centerX, centerY, 'TitleScreen').setOrigin(0.5);
+        //Creating interactable images
+        let play = this.add.image(centerX/2, centerY + textSpacer, 'Play').setOrigin(0.5);
+        let settings = this.add.image(centerX/2, centerY + textSpacer * 2, 'Settings').setOrigin(0.5);
+        //Making images interactable
+        play.setInteractive();
+        settings.setInteractive();
+        //Setting interactive behaviors
+        play.on('pointerover', () => play.setFrame(1));
+        play.on("pointerout", () => play.setFrame(0));
+        play.on('pointerup', () => {this.scene.start('playScene');});
+        settings.on('pointerover', () => settings.setFrame(1));
+        settings.on("pointerout", () => settings.setFrame(0));
+        settings.on('pointerup', () => {this.scene.start('settingsScene');});
     }
 
     update() {
-        if(Phaser.Input.Keyboard.JustDown(keyLEFT)) {
-            this.sound.play("sfx_select");
-            this.scene.start("playScene");
-        }
     }
 }
