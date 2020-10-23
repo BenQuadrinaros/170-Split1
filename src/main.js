632d305483ea691548054b16ea94169bd64d26a1
Mark <35475840+omegaman123@users.noreply.github.com>
2020-10-23 12:03:58 -0700

    small hub scene implementation

--- src/main.js
+++ src/main.js
@@ -4,10 +4,13 @@ let config = {
     type: Phaser.CANVAS,
     width: 640,
     height: 480,
-    scene: [Menu, Play, Settings, Map]
+    scene: [Menu, Play, Settings, Map, Hub]
 };
 
 let game = new Phaser.Game(config);
 
 //reserve some keyboard variables
-let keyP, keyLEFT, keyRIGHT;
+
+
+let keyP, keyESCAPE, keyLEFT, keyRIGHT, keyDOWN, keyUP, keySPACE;
+
---
+++ src/prefabs/HubPlayer.js
@@ -0,0 +1,40 @@
+class HubPlayer extends Phaser.GameObjects.Sprite {
+
+    constructor(scene, texture, frame, initX, initY) {
+        super(scene, initX, initY, texture, frame);
+        scene.add.existing(this)
+        this.setPosition(initX, initY)
+    }
+
+
+    update(){
+        if (keyLEFT.isDown){
+            this.flipX = false;
+            if (this.x > 0){
+                this.x -= 1;
+            }
+        }
+
+        if (keyRIGHT.isDown){
+            this.flipX = true;
+            if (this.x < config.width){
+                this.x += 1;
+            }
+        }
+
+        if (keyUP.isDown){
+            if (this.y > 0){
+                this.y -= 1;
+            }
+        }
+
+        if (keyDOWN.isDown){
+            if (this.y < config.height){
+                this.y += 1;
+            }
+        }
+
+
+    }
+
+}
