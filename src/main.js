2e17850ae8ea4f1f015d1327f8699570c8c0d750
Carl Erez <cerez@ucsc.edu>
2020-11-21 01:21:43 -0800

    Added in gardening, game is up to date with glitch

--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@ let config = {
     type: Phaser.CANVAS,
     width: 640,
     height: 480,
-    scene: [Menu, Play, Settings, Map, Hub],
+    scene: [Menu, Play, Settings, Map, Garden, Hub],
     volume: .7
 };
 
@@ -13,5 +13,6 @@ let game = new Phaser.Game(config);
 //reserve some keyboard variables
 let keyP, keyESCAPE, keyLEFT, keyRIGHT, keyDOWN, keyUP, keySPACE;
 
-let upgrades = {"bike": 0, "bee": 0, "tools": 0}
+let upgrades = {"bike": 0, "bee": 0, "tools": 0};
+let plants = [3, -1, -1, -1]; //-1 is unbought, 0 is dead, 0.5 and 1 are poor, 1.5 and 2 are good, 2.5 and 3 are great
 
