8b92b01e97874f46bc8eac68dc38170674323b34
Mark <35475840+omegaman123@users.noreply.github.com>
2021-01-12 21:33:06 -0800

    Obstacle implementation with incorrect positioning + Collision detection.

--- src/prefabs/Obstacle.js
+++ src/prefabs/Obstacle.js
@@ -1,15 +1,21 @@
 class Obstacle extends Phaser.GameObjects.Sprite {
     constructor(scene, initx, inity, texture, frame, speed) {
-        super(scene, initX, initY, texture, frame);
+        super(scene, initx, inity, texture, frame);
         scene.add.existing(this);
         scene.physics.add.existing(this);
         this.speed = speed;
-        this.setPosition(initX, initY);
+        this.setPosition(initx, inity);
+        this.lane = 'None'
+        this.startPoint = initx;
     }
 
 
 
     update(){
-        this.x += this.speed;
+        this.x -= this.speed;
+        //console.log("I am an obstale at : " + this.x)
+        if (this.x < -10){
+            this.x = this.startPoint;
+        }
     }
 }
