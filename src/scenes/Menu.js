82d7bee854723758f57e5e7c08d28b066c748f8d
Carl Erez <cerez@ucsc.edu>
2021-01-26 10:30:04 -0800

    Added keyboard functionality to main menu

--- src/scenes/Menu.js
+++ src/scenes/Menu.js
@@ -1,6 +1,8 @@
 class Menu extends Phaser.Scene {
     constructor() {
         super("menuScene");
+
+        this.currSelected = -1;
     }
 
 
@@ -12,29 +14,49 @@ class Menu extends Phaser.Scene {
         let textSpacer = 64;
 
         //Setting controls
-        keyLEFT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.LEFT);
-        keyRIGHT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.RIGHT);
+        keyUP = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.UP);
+        keyDOWN = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.DOWN);
+        this.keyENTER = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.ENTER);
 
         //Setting Background
         this.menu = this.add.image(centerX, centerY, 'TitleScreen').setOrigin(0.5);
         //Creating interactable images
-        let play = this.add.image(centerX/2, centerY + textSpacer, 'Play').setOrigin(0.5);
-        let settings = this.add.image(centerX/2, centerY + textSpacer * 2, 'Settings').setOrigin(0.5);
+        this.play = this.add.image(centerX/2, centerY + textSpacer, 'Play').setOrigin(0.5);
+        this.settings = this.add.image(centerX/2, centerY + textSpacer * 2, 'Settings').setOrigin(0.5);
+        
+        //Create selection change event
+        this.events.on("selectionChange", this.selectionUpdated, this);
+
         //Making images interactable
-        play.setInteractive();
-        settings.setInteractive();
+        this.play.setInteractive();
+        this.settings.setInteractive();
         //Setting interactive behaviors
-        play.on('pointerover', () => play.setFrame(1));
-        play.on("pointerout", () => play.setFrame(0));
-        play.on('pointerup', () => {
-            this.music.stop();
-            this.scene.start('hubScene');
+        this.play.on('pointerover', () => {
+            this.currSelected = 1;
+            this.events.emit("selectionChange");
         });
-        settings.on('pointerover', () => settings.setFrame(1));
-        settings.on("pointerout", () => settings.setFrame(0));
-        settings.on('pointerup', () => {
-            this.music.stop();
-            this.scene.start('settingsScene');
+        this.play.on("pointerout", () => {
+            this.currSelected = -1;
+            this.events.emit("selectionChange");
+        });
+            
+        this.play.on('pointerup', () => {
+            //this.music.stop();
+            //this.scene.start('hubScene');
+            this.moveToNewScene(1);
+        });
+        this.settings.on('pointerover', () => {
+            this.currSelected = 2;
+            this.events.emit("selectionChange");
+        });
+        this.settings.on("pointerout", () => {
+            this.currSelected = -1;
+            this.events.emit("selectionChange");
+        });
+        this.settings.on('pointerup', () => {
+            //this.music.stop();
+            //this.scene.start('settingsScene');
+            this.moveToNewScene(2);
         });
 
         //background music for the menu
@@ -45,5 +67,77 @@ class Menu extends Phaser.Scene {
     }
 
     update() {
+        if (Phaser.Input.Keyboard.JustDown(keyUP)) {
+            if(this.currSelected === -1){
+                this.currSelected = 1;
+                this.events.emit("selectionChange");
+            }
+            else{
+                this.currSelected -= 1;
+                if(this.currSelected === 0){
+                    this.currSelected = 2;
+                }
+                this.events.emit("selectionChange");
+            }
+        }
+
+        if (Phaser.Input.Keyboard.JustDown(keyDOWN)) {
+            if(this.currSelected === -1){
+                this.currSelected = 1;
+                this.events.emit("selectionChange");
+            }
+            else{
+                this.currSelected += 1;
+                if(this.currSelected === 3){
+                    this.currSelected = 1;
+                }
+                this.events.emit("selectionChange");
+            }
+        }
+
+        if(Phaser.Input.Keyboard.JustDown(this.keyENTER)){
+            if(this.currSelected === -1){
+                //No effect if nothing selected
+            }
+            else{
+                this.moveToNewScene(this.currSelected);
+            }
+        }
+    }
+
+    selectionUpdated(){
+        //For each option, set the frame properly
+        console.log("Selection Updated: " + this.currSelected);
+        //Play
+        if(this.currSelected === 1){
+            this.play.setFrame(1);
+        }
+        else{
+            this.play.setFrame(0);
+        }
+
+        //Settings
+        if(this.currSelected === 2){
+            this.settings.setFrame(1);
+        }
+        else{
+            this.settings.setFrame(0);
+        }
+    }
+
+    moveToNewScene(newScene){
+        this.music.stop();
+        //Play is being pressed
+        if(newScene === 1){
+            this.scene.start('hubScene');
+        }
+        //Settings is being pressed
+        else if(newScene === 2){
+            this.scene.start('settingsScene');
+        }
+        //Other result
+        else{
+            console.log("Invalid Scene selected: " + newScene);
+        }
     }
 }
